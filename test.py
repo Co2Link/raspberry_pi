715479996be095f0894c2b0965e7da865d1487ac
lingxt <lxt9601@gmail.com>
2019-12-18 18:48:55 +0900

    sync

--- test.py
+++ test.py
@@ -1,24 +1,13 @@
-import io
-import socket
-import struct
-import time
-import picamera
+a = [1,2,3]
 
-try:
-    camera = picamera.PiCamera()
-    camera.resolution = (640,480)
-    camera.start_preview()
-    time.sleep(2)
 
-    start = time.time()
-    stream = io.BytesIO()
+def tdfdf():
+    for i in a:
+        yield i
 
-    for foo in camera.capture_continuous(stream,'jpeg'):
-        a = stream.tell()
-        s = struct.pack('<L',a)
-        print(a)
-        print(s)
-        print(type(a))
-        print(type(s))
-finally:
-    pass
+c = tdfdf()
+
+
+next(c)
+next(c)
+next(c)
