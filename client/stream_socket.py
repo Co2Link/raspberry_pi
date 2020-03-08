1bcf89ef8c8f40bb068defe6b9a4ed7b19293d05
lingxt <lxt9601@gmail.com>
2020-03-07 22:43:38 -1000

    update stream_socket.py

--- client/stream_socket.py
+++ client/stream_socket.py
@@ -3,6 +3,17 @@ import socket
 import struct
 import time
 import picamera
+import threading
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--resolution',type=str,default='640x480',help='WxH')
+parser.add_argument('--frame_rate',type=int,default=30)
+
+args = parser.parse_args()
+
+RESOLUTION = args.resolution
+FRAME_RATE = args.frame_rate
 
 class SplitFrames(object):
     def __init__(self, connection):
@@ -24,25 +35,30 @@ class SplitFrames(object):
                 self.stream.seek(0)
         self.stream.write(buf)
 
+def listenToClient(client,address):
+    connection = client.makefile('wb')
+    try:
+        output = SplitFrames(connection)
+        with picamera.PiCamera(resolution=RESOLUTION, framerate=FRAME_RATE) as camera:
+            time.sleep(2)
+            start = time.time()
+            camera.start_recording(output, format='mjpeg')
+            camera.wait_recording(1000000)
+    except ConnectionResetError as e:
+        print('connection closed by client({})'.format(address))
+        client.close()
+        camera.close()
+    finally:
+        finish = time.time()
+    print('Sent %d images in %d seconds at %.2ffps' % (
+        output.count, finish-start, output.count / (finish-start)))
+
 server_socket = socket.socket()
 server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 server_socket.bind(('0.0.0.0', 8000))
 server_socket.listen(0)
-connection = server_socket.accept()[0].makefile('wb')
-try:
-    output = SplitFrames(connection)
-    with picamera.PiCamera(resolution='VGA', framerate=30) as camera:
-        time.sleep(2)
-        start = time.time()
-        camera.start_recording(output, format='mjpeg')
-        camera.wait_recording(1000000)
-        # camera.stop_recording()
-        # Write the terminating 0-length to the connection to let the
-        # server know we're done
-        # connection.write(struct.pack('<L', 0))
-finally:
-    connection.close()
-    server_socket.close()
-    finish = time.time()
-print('Sent %d images in %d seconds at %.2ffps' % (
-    output.count, finish-start, output.count / (finish-start)))
+
+while True:
+    client, address = server_socket.accept()
+    threading.Thread(target=listenToClient,args=(client,address)).start()
+
